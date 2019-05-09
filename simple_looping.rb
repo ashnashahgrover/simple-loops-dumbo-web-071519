981c0f6ce7582a914dc79558b889dfa599b198dc
Ashna Shah-Grover <as19@williams.edu>
2019-05-09 19:16:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,6 +14,7 @@ def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  
 end
 
 def while_iterator(number_of_times)
